9d77089ed1017e0294285f6b5ebf43d00fc2a0c6
repl.it user <replituser@example.com>
2021-02-28 11:31:35 +0000

    addesd messages

---
+++ basics/output/multiline_message.py
@@ -0,0 +1,7 @@
+#display message to standard in multiple lines of code 
+print("System Failure Iminent")
+print("System reboot has been initiated...")
+print("...tebooting sensory system")
+print("...rebooting output motors")
+print("...rebooting hover engine\n")
+print("System online")
--- main.py
+++ main.py
@@ -1,1 +1,4 @@
 print("Hello")
+print("first step:git add.")
+print("second step:git commit -m")
+print("last step:\"git push\"")
