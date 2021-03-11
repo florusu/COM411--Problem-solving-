5fd715bb7c5595a65785e6f9c9bbccc1bc4ee8e8
repl.it user <replituser@example.com>
2021-03-11 21:09:09 +0000

    beep

--- basics/output/escape_characters.py
+++ basics/output/escape_characters.py
@@ -2,4 +2,5 @@ print("\nDisplays new line")
 print("\t displays a tab space")
 print("\\displays a backslash")
 print("\"displays a double quote")
-print("\"displays single quote")
+print("\'displays single quote")
+
--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-print("how many times to print the symbol ?")
-x=int(input())
-#i is a counter it keeps track of how many times we went through the Loop
-i =0
-
-while i < x: #condition for repeating the code as i lower than x
-  print("\u27BD")
-  i=i+1 #new value of the counter is one more than it used to be 
-  print("we left the loop")
+print("Hello")
+print("first step:git add.")
+print("second step:git commit -m")
+print("last step:\"git push\"")
